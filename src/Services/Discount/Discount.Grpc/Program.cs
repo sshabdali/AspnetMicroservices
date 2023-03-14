8e2a67c625f26a15820c0bfd07b6043edb184151
sshabdali <sshabdali@hotmail.com>
2023-03-14 13:02:15 +0000

    Added Discount.Grpc server

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,21 @@
+using Discount.Grpc.Extensions;
+
+namespace Discount.Grpc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<Program>();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
